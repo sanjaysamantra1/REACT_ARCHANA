d241d30d71f8ee3677bf1559e5962dce8098fb52
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-03-10 10:02:31 +0530

    day 3

--- react-project-1/src/App.js
+++ react-project-1/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar'
+import Carousel from './components/carousel/Carousel'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <Navbar />
+    <Carousel />
+    <Main />
+    <Footer />
+  </>
 }
 
 export default App;
