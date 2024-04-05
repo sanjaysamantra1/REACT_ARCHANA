52bd3e6146eb7b9da13f3ec36e397449ed578bf5
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-05 07:51:55 +0530

    pagination code

--- react-project-1/src/components/products/ProductList.js
+++ react-project-1/src/components/products/ProductList.js
@@ -2,9 +2,24 @@ import React, { useState } from 'react';
 import productsArr from './products.json';
 import ProductCard from './ProductCard';
 import SearchBar from 'react-js-search';
+import ReactPaginate from 'react-paginate';
+import './Pagination.css'
 
 export default function ProductList() {
-    const [filteredProducts, setFilteredProducts] = useState(productsArr);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 4;
+
+    const endOffset = itemOffset + itemsPerPage;
+    const [filteredProducts, setFilteredProducts] = useState(productsArr.slice(itemOffset, endOffset));
+    const pageCount = Math.ceil(productsArr.length / itemsPerPage);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % productsArr.length;
+        setItemOffset(newOffset);
+        const endOffset = newOffset + itemsPerPage;
+        const newDataSet = productsArr.slice(newOffset, endOffset);
+        setFilteredProducts([...newDataSet]);
+    };
 
     const onSearchChange = function (term, filteredData) {
         setFilteredProducts(filteredData);
@@ -19,7 +34,7 @@ export default function ProductList() {
     }
     return <>
         <div className='container'>
-            <div className='row my-1'>
+            <div className='row'>
                 <div className='col-sm-8'>
                     <SearchBar
                         onSearchTextChange={(term, filteredData) => { onSearchChange(term, filteredData) }}
@@ -33,10 +48,26 @@ export default function ProductList() {
                     <button onClick={sortDesc} className='btn btn-secondary'>Sort Desc</button>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row my-1'>
                 {filteredProducts.map(product => {
                     return <ProductCard product={product} key={product.id} />
                 })}
+            </div>
+            <div className='row'>
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    renderOnZeroPageCount={null}
+                    containerClassName="pagination"
+                    pageLinkClassName="page-num"
+                    previousLinkClassName="page-num"
+                    nextLinkClassName="page-num"
+                    activeLinkClassName="active"
+                />
             </div>
         </div>
     </>
